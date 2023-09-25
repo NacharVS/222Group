ffd593e100193dd7415db0fb1903f6c1020d9371
r3dinc <r3dinc@yandex.ru>
2023-09-25 14:27:55 +0300

    q

---
+++ TestConsole/SummId.cs
@@ -0,0 +1,14 @@
+﻿namespace TestConsole
+{
+    class SummId
+    {
+        public int id;
+        public int summ;
+
+        public SummId()
+        {
+            id = 1;
+            summ = 0;
+        }
+    }
+}
---
+++ TestConsole/User.cs
@@ -0,0 +1,39 @@
+﻿namespace TestConsole
+{
+    class User 
+    {
+      
+        
+        private int id;
+        private string name;
+        private int age;
+
+        public User()
+        {
+
+        }
+        public User(string name, int age)
+        {
+            id = 0;
+            this.name = name;
+            this.age = age;
+        }
+
+        public User(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Id: {id} Name: {name} Age: {age}");
+        }
+
+        public void ChangeName(string newname)
+        {
+            name = newname;
+        }
+    }
+}
