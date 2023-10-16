b7c70ecea345b4d84d1d051f22e07a549830cf32
Aksenov Danya <gkate4412@gmail.com>
2023-10-16 18:42:24 +0300

    Начал делать дуэль

--- Units 222 Draft/Program.cs
+++ Units 222 Draft/Program.cs
@@ -1,2 +1,6 @@
 ﻿using UnitsDrafts;
 Console.WriteLine("пососал");
+var th = new Townhall();
+var bh = th.SpawnBishop();
+var ps = th.SpawnPeasant();
+fight.duel(bh, ps);
--- Units 222 Draft/fight.cs
+++ Units 222 Draft/fight.cs
@@ -11,9 +11,21 @@ namespace UnitsDrafts
     {
         public static void duel(Unit unit1, Unit unit2)
         {
-            while (unit1.Alive!= false ||  unit2.Alive!= false)
+            Unit[] duel_spisok = new Unit[2] {unit1,unit2};
+            Unit[] duel_spisok2 = new Unit[2] { unit2, unit1 };
+            int count = 0;
+            while (true)
             {
-                
+                if (unit1.Alive != false && unit2.Alive != false)
+                {
+                    for (int i = 0; i < 2; i++)
+                    {
+                        duel_spisok[i].DealDamage(duel_spisok2[i]);
+                        count++;
+                        Console.WriteLine($"бой закончился за {count} ходов");
+                    }
+                }
+                else { break; }
             }
         }
     }
