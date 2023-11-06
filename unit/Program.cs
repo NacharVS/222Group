89e5c8f1aa1e6606550d06b99272e5051bc07829
Farrahov Ildar <farrahovi2006@gmail.com>
2023-11-06 20:23:54 +0300

    Добавил атаку для башни с лучниками и добавил сортировку оружия по урону.

--- unit/Program.cs
+++ unit/Program.cs
@@ -16,14 +16,20 @@ var ft2 = barracs1.CreateFootman();
 var archer = barracs1.CreateArchcer();
 var bishop = meriya.CreateBishop();
 var peasant = meriya.CreatePeasant();
-
+WatchingTowers wt = new WatchingTowers();
 
 Sword sword = new Sword();
-sword.ShowWeaponInfo();
-sword.LevelUp();
-sword.ShowWeaponInfo();
-Console.WriteLine();
+//sword.ShowWeaponInfo();
+//sword.LevelUp();
+//sword.ShowWeaponInfo();
+//Console.WriteLine();
 
+ft1.BaseInfo();
+while (ft1.Health>0)
+{
+    wt.TowerDamage(ft1);
+    ft1.BaseInfo();
+}
 
 while (true)
 {
@@ -40,7 +46,8 @@ while (true)
         " \n 7.Дуэль " +
         "\n 8.Archer Dead check " +
         "\n 9.Footman Dead check " +
-        "\n 10.ArcherTower");
+        "\n 10.ArcherTower" +
+        "\n 11.Gnome Sort");
     Console.WriteLine();
     Console.ResetColor();
     int choice = int.Parse(Console.ReadLine());
@@ -271,7 +278,7 @@ while (true)
     }
     if (choice == 10)
     {
-        WatchingTowers wt = new WatchingTowers();
+        
         wt.UnitsCount();
         wt.ShowTowerUnits();
         wt.Upgrade();
@@ -279,7 +286,55 @@ while (true)
         wt.UnitsCount();
 
     }
+    Axe axe = new Axe(); Bow bow = new Bow(); Mace mace = new Mace();
+    if (choice == 11)
+    {
+        
+        var array = new Weapon[4] { sword, axe, bow, mace };
+        var ildar = GnomeSort(array);
+
+        foreach (Weapon weapon in ildar)
+        {
+            Console.WriteLine(weapon.WeaponName + " " + weapon.MinDamage);
+        }
+
+        static void Swap(ref Weapon item1, ref Weapon item2)
+        {
+            var temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+        //Гномья сортировка
+        static Weapon[] GnomeSort(Weapon[] unsortedArray)
+        {
+            var index = 1;
+            var nextIndex = index + 1;
+
+            while (index < unsortedArray.Length)
+            {
+                if (unsortedArray[index - 1].MinDamage < unsortedArray[index].MinDamage)
+                {
+                    index = nextIndex;
+                    nextIndex++;
+                }
+                else
+                {
+                    Swap(ref unsortedArray[index - 1], ref unsortedArray[index]);
+                    index--;
+                    if (index == 0)
+                    {
+                        index = nextIndex;
+                        nextIndex++;
+                    }
+                }
+            }
+
+            return unsortedArray;
+        }
+    }
 }
+
 
 
 
--- unit/WatchingTowers.cs
+++ unit/WatchingTowers.cs
@@ -4,35 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
+using unit.Items;
+using System.Reflection.Emit;
 
 namespace unit
 {
     internal class WatchingTowers
     {
-        List<Unit> Garrison = new List<Unit>() { new Archer(), new Archer(), new Archer()};
+        List<Unit> Garrison = new List<Unit>() { new Archer(), new Archer(), new Archer() };
         static int LevelCount = 0;
-         
-        
-            
-            
+        static int TowerAccuracy = 40;
+        //static int TowerCrit = 20;
+
+
         public void ShowTowerUnits()
         {
             Console.WriteLine();
             Console.WriteLine("Стрелки в башне");
             foreach (var unit in Garrison)
             {
-               unit.BaseInfo();
+                unit.BaseInfo();
             }
-            
-        }
-        public void TowerDamage()
-        {
 
+        }
+        public void TowerDamage(Unit unit)
+
+        {
+            if (unit.Alive)
+            {
+                if (unit.Health > 0)
+                {
+                    int x = new Random().Next(1, 101);
+                    if (x <= TowerAccuracy)
+                    {
+                        double Damage = new Random().Next(5, 5 * Garrison.Count);
+                        unit.Health -= Damage;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Garrison сделал выстрел, здоровье {unit.Name}: {unit.Health}/{unit.MaxHealth} ");
+
+
+                        Console.ResetColor();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Башня не попала");
+                    }
+                }
+
+                if (unit.Health <= 0)
+                {
+
+                    Stat.KilledQuant++;
+                    unit.Alive = false;
+                    Console.WriteLine($"Garrison не может больше стрелять так как {unit.Name} мертв");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Юнит мертв");
+                
+            }
         }
         public void Upgrade()
         {
             Garrison.Add(new Archer());
-            
+
             LevelCount++;
             Console.WriteLine($"Уровень башни был повышен, текущий уровень {LevelCount}");
         }
