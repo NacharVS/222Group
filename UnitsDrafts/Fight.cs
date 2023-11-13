eefc5fd6bf46c17df37d12625d34d91a4a430543
Blur72 <adel.shaihutdinov123@gmail.com>
2023-11-13 15:05:08 +0300

    изменил арену

--- UnitsDrafts/Fight.cs
+++ UnitsDrafts/Fight.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UnitsDrafts
 {
-    internal static class Fight
+    internal class Fight
      
     {
+        public Archer adelianzak = new Archer("adelian lox", 40, 11, false);
+        public Footman adeliangraves = new Footman("adelian lox", 60, 10, true);
         
-        public static void Arenavoins()
+        public void Arenavoins()
         {
-            string vibor1 = Console.ReadLine();
             Console.WriteLine("Выберите либо Мечник или Лучник");
+            string vibor1 = Console.ReadLine();
             if (vibor1 == "Лучник")
             {
-                Archer adelianpodsos = new Archer("adelian lox", 40, 11, false);
-                Console.WriteLine($"Ваш раб готов к битве и его имя{adelianpodsos.Name}");
+                Console.WriteLine($"Ваш раб готов к битве и его имя{adelianzak.Name}");
+            }
+            if (vibor1 == "Мечник")
+            {
+                Console.WriteLine($"Ваш раб готов к битве и его имя{adeliangraves.Name}");
             }
             else
             {
-                Footman adellol = new Footman("adelian lox", 60, 10, true);
-                Console.WriteLine($"Ваш раб готов к битве и его имя{adellol.Name}");
+                Console.WriteLine("Такого нет");
+                return;
             }
             Console.WriteLine("Выберите действие\n 1.Нанести урон\n 2.Сдаться\n 3.Самоубийство(показать возможности)");
+            var vibor2 = Convert.ToInt32(Console.ReadLine());
+            if (vibor2 == 1)
+            {
+                
+            }
+
 
         }
         
