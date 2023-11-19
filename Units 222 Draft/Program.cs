63bcebbf9d7d1eab3891ecb4db52178890e63732
Aksenov Danya <gkate4412@gmail.com>
2023-11-19 20:00:38 +0300

    имена юнитам дал

--- Units 222 Draft/Program.cs
+++ Units 222 Draft/Program.cs
@@ -7,12 +7,14 @@ Console.WriteLine("пососал");
 var th = new Townhall();
 var br = new Barracs();
 var bh = th.SpawnBishop();
-var ft = br.SpawnFootman(); ft.Name = "Василиса";
-var ft1 = br.SpawnFootman(); ft1.Name = "Марина";
-var ft2 = br.SpawnFootman(); ft2.Name = "Савелий";
-var ft3 = br.SpawnFootman(); ft3.Name = "Даниил";
+var ft = br.SpawnFootman(); ft.Name = "Биба";
+//даня аксенов сделал кстати
+var ft1 = br.SpawnFootman(); ft1.Name = "Боба";
+var ft2 = br.SpawnFootman(); ft2.Name = "Пупа";
+var ft3 = br.SpawnFootman(); ft3.Name = "Лупа";
 
-List<Unit> Team1 = new List<Unit>() {ft1,ft };
-List<Unit> Team2 = new List<Unit>() {ft2,ft3 };
+List<Unit> Team1 = new List<Unit>() { ft1, ft };
+List<Unit> Team2 = new List<Unit>() { ft2, ft3 };
 //Fight.Duel(ft, ft1);
 Fight.War(Team1, Team2);
+
