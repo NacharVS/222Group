daf195d72e4ed0222c7b7885e9a6caf45a35c9be
Obeziyana228 <ivan.ser.mironov@gmail.com>
2023-11-23 16:47:24 +0300

    Weapon работы с атакой

--- UnitsDrafts/Weapon.cs
+++ UnitsDrafts/Weapon.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,24 +10,41 @@ namespace UnitsDrafts
 {
     internal class Weapon
     {
-        public Weapon(string name, int minDam, int maxDam, int attackSpeed, int accuracy, int durability, bool Bleed)
+        public Weapon(string name, int maxDamage, int mindamage, int accuracy, int durability, int attackspeed, bool Krovotok)
         {
-            WepName = name; MinDamage = minDam;
-            MaxDamage = maxDam; AttackSpeed = attackSpeed;
-            Accuracy = accuracy; Durability = durability;
-            Bleede = Bleed;
+            Wepname = name;
+            MaxDam = maxDamage;
+            MinDam = mindamage;
+            Acc = accuracy;
+            Dura = durability;
+            AttSp = attackspeed;
+            Krovyaka = Krovotok;
         }
-        public string WepName { get; set; }
-        public int MinDamage { get; set; }
-        public int MaxDamage { get; set; }
-        public int AttackSpeed { get; set; }
-        public int Accuracy { get; set; }
-        public int Durability { get; set; }
-        public bool Bleede { get; set; } = false;
-    }
-    public u tichka(Unit unit)
-    {
-        
-    }
+        public string Wepname { get; set; }
+        public int MaxDam { get; set; }
+        public int MinDam { get; set; }
+        public int Acc { get; set; }
+        public int Dura { get; set; }
+        public int AttSp { get; set; }
 
+        public bool Krovyaka { get; set; } = false; 
+        public int Metodataki(Unit unit)
+        {
+            if (AttSp < 4)
+            {
+                Console.WriteLine("Вы натянули тетеву");
+                Thread.Sleep(5000);
+                var x = new Random().Next(1, 101);
+                if (x <= Acc && Dura != 0)
+                {
+                    Console.W
+                }
+
+            }
+        }
+
+
+    }
 }
+    
+   
