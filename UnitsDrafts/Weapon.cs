7ec5cebdb826b96d030a4c10e6b6c8f055670175
l0pn0 <lopno228@gmail.com>
2023-11-23 21:51:55 +0300

    работа с weapon

--- UnitsDrafts/Weapon.cs
+++ UnitsDrafts/Weapon.cs
@@ -27,23 +27,95 @@ namespace UnitsDrafts
         public int Dura { get; set; }
         public int AttSp { get; set; }
 
-        public bool Krovyaka { get; set; } = false; 
+        public bool Krovyaka { get; set; } = false;
         public int Metodataki(Unit unit)
         {
-            if (AttSp < 4)
+            if (AttSp < 3)
             {
+                Thread.Sleep(2000);
                 Console.WriteLine("Вы натянули тетеву");
-                Thread.Sleep(5000);
+                Thread.Sleep(6000);
+                Console.WriteLine("Выстрел!!!");
                 var x = new Random().Next(1, 101);
                 if (x <= Acc && Dura != 0)
                 {
-                    Console.W
+                    Console.WriteLine("Вы попали в цель");
+                    Dura -= 8;
+                    // возможно стоит добавить макс дура
+                    unit.Health = unit.Health - new Random().Next(MinDam, MaxDam + 1);
+                    //Нужно сделать урон по юнита
+                    // + точность
                 }
+                else
+                {
+                    Console.WriteLine("Вы промахнулись");
+                    Dura -= 8;
+                    //- точность
+                    return 0;
+                }
+            }
+            else if (AttSp < 6 && AttSp < 3)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Вы замахнулись буловой");
+                Thread.Sleep(4500);
+                var x = new Random().Next(1, 101);
+                if (x <= Acc && Dura != 0)
+                {
+                    Console.WriteLine("Вы ударили по цели");
+                    Dura -= 15;
+                    unit.Health = unit.Health - new Random().Next(MinDam, MaxDam + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Вы не смогли ударить по цели");
+                    return 0;
+                }
+            }
+            else if (AttSp > 5 && AttSp < 8)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Вы замахнулись топором");
+                Thread.Sleep(3000);
+                var x = new Random().Next(1, 101);
+                if (x <= Acc && Dura != 0)
+                {
+                    Console.WriteLine("Вы ударили по цели");
+                    Dura -= 10;
+                    unit.Health = unit.Health - new Random().Next(MinDam, MaxDam + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Вы не смогли ударить по цели");
+                    return 0;
+                }
+            }
+            else if (AttSp < 10 && AttSp < 7)
+            {
+                Console.WriteLine("Вы замахнулись мечом");
+                Thread.Sleep(1000);
+                var x = new Random().Next(1, 101);
+                if (x <= Acc && Dura != 0)
+                {
+                    Console.WriteLine("Вы ударили по цели");
+                    Dura -= 5;
+                    unit.Health = unit.Health - new Random().Next(MinDam, MaxDam + 1);
+                }
+                else
+                {
+                    Console.WriteLine("Вы не смогли ударить по цели");
+                    return 0;
+                }
+
+                //// сделать кровотечение  for (int i = 5; i >= 0; i--)
+                //{
+                //    Thread.Sleep(1000);
+                //    unit.Health = unit.Health - new Random().Next(1, 5);
+                //    добавить инфу от юнито при каждом действии unit.BaseInfo();
+                //}
 
             }
         }
-
-
     }
 }
     
