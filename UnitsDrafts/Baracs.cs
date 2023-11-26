ef21f1071e565db07b3c608446ab8c05bf9decfb
l0pn0 <lopno228@gmail.com>
2023-11-26 21:38:05 +0300

    Бараки

---
+++ UnitsDrafts/Baracs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal static class Baracs
+
+    {
+        public static Footman SozdatPehotintsa()
+        {
+            Statistics.FootmansCount++;
+            Footman footman = new Footman();
+            return footman;
+        }
+        public static Archer SozdatLuchnika()
+        {
+            Statistics.ArchersCount++;
+            Archer archer = new Archer();
+            return archer;
+        }
+    }
+}
