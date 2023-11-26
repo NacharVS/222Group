7c079a0fb804b4b6b0a9334b758b41206b9b8ad1
KamilIshkeev <148053974+KamilIshkeev@users.noreply.github.com>
2023-11-26 13:50:51 +0300

    Add files via upload

---
+++ Сишарп/UnitsDrafts/Items/Mace.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts.Items
+{
+    internal class Mace : Weapon
+    {
+       
+        public Mace() : base("Mace", 2, 10, 30, 60, 100, false, 40)
+        {
+        }
+        public override double Hit(Unit unit)
+        {
+            if (Durability > 0)
+            {
+                double damage = new Random().Next(MinDamage, MaxDamage);
+                Durability = Durability - 2;
+                int y = new Random().Next(1, 100);
+                if (Accuracy >= y)
+                {
+                    damage += lvl * 5;
+                    int x = new Random().Next(1, 100);
+                    if (x <= StChance)
+                    {
+                        Console.WriteLine("Противник оглушен");
+                        unit.StChance = true;
+                    }
+                    return damage * Durability_check();
+                }
+                else
+                {
+                    Console.WriteLine("Промах");
+                    return 0;
+                }
+            }
+            Console.WriteLine("Оружие сломалось!");
+            return 0;
+        }
+    }
+}
---
+++ Сишарп/UnitsDrafts/Items/Sword.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal class Sword : Weapon
+    {
+        private Weapon _weapon;
+        public double CritDamage = 1.5;
+        public double CritChance = 20;
+
+        public Sword() : base("Sword", 2, 7, 20, 60, 100, false, 0)
+        {
+
+        }
+
+
+
+
+
+        public override double Hit(Unit unit)
+        {
+            if (Durability > 0)
+            {
+                Durability -= 2;
+                double damage = new Random().Next(MinDamage, MaxDamage);
+                var y = new Random().Next(1, 101);
+                if (Accuracy >= y)
+                {
+                    damage += lvl * 5;
+                    var x = new Random().Next(1, 101);
+                    if (x <= CritChance)
+                    {
+                        damage *= CritDamage;
+                        Console.WriteLine("Крит урон!");
+                        return damage;
+                    }
+                    return damage * Durability_check();
+                }
+                else
+                {
+                    Console.WriteLine("Промах!");
+                    return 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Оружие сломалось!");
+                return 0;
+            }
+        }
+
+
+        public virtual float Crit()
+        {
+            var x = new Random().Next(1, 101);
+            if (x <= CritChance)
+            {
+                return new Random().Next(MinDamage, MaxDamage);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
