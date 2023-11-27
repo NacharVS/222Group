b0a5606b5d018784b4ad9a7b4a4e2b61b5776407
l0pn0 <lopno228@gmail.com>
2023-11-27 13:38:55 +0300

    ТОВЫРЫА

---
+++ UnitsDrafts/Towers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace UnitsDrafts
+//{
+//    internal class Towers
+//    {
+//        private string _twname;
+//        private int _twhealth;
+//        private int _twmaxhealth;
+//        public string TWname
+//        {
+//            get { return _twname; }
+//            set { _twname = value; }
+//        }
+//        public int Twhealth 
+//        {
+//            get { return _twhealth; }
+//            set { _twhealth = value; }
+//        }
+//        public int Twmaxhealth
+//        {
+//            get { return _twmaxhealth; }
+//            set { _twmaxhealth = value;
+//                if (value > Twmaxhealth)
+//            }   
+//        }
+//        public Towers(string TowerName, int TowerHealth, int TowerMaxHealth)
+//        {
+//            TWname = TowerName;
+//            Twhealth = TowerHealth;
+//            Twmaxhealth = TowerMaxHealth;
+//        }
+
+//    }
+//}
