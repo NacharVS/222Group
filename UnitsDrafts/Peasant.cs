ce0025a9b90f4dec75fe3badab9a3e982b658486
KamilIshkeev <148053974+KamilIshkeev@users.noreply.github.com>
2023-12-04 14:13:02 +0300

    Add files via upload

---
+++ UnitsDrafts/Peasant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal class Peasant : Unit
+    {
+        public Peasant() : base("Peasant", 30, 2, 5, 5)
+        {
+        }
+
+
+    }
+}
