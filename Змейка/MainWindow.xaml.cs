3ddcf605e4e2420d999e7bf2188905025502e57a
Malik Valeev <malik.mvi2005@gmail.com>
2024-01-16 00:55:43 +0300

    Змейка. Сделал игровое поле, понял как выводить новые элементы через код на конкретное полжение по индексу.

---
+++ Змейка/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Змейка
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        public void SnakeInit()
+        {
+  
+            Rectangle SnakeHead = new Rectangle();
+            SnakeHead.Width = 53;
+            SnakeHead.Height = 53;
+            SnakeHead.Fill = Brushes.Green;
+
+            GameField.Children.Add(SnakeHead);
+            Grid.SetColumn(SnakeHead, 4);
+            Grid.SetRow(SnakeHead, 4);
+            
+        }
+
+        private void Btn_Start(object sender, RoutedEventArgs e)
+        {
+            SnakeInit();
+            btnStart.IsEnabled = false;  
+        }
+    }
+}
