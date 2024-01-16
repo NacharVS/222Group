35aa042c8e9d6798c8573565c4bd169a38fd837e
Murin <murinkd@gmail.com>
2024-01-16 13:20:15 +0300

    ---

---
+++ ФормаСписка/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ФормаСписка
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<UserInfo> users;
+
+        public MainWindow()
+        {
+            users = new List<UserInfo>();
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            users.Add(new(txtName.Text, txtSurname.Text));
+            txtName.Clear();
+            txtSurname.Clear();
+            ListBoxRefresh();
+        }
+
+        private void ListBoxRefresh()
+        {
+
+            foreach (UserInfo user in users)
+            {
+                listBox.Items.Add(user.Surname);
+            }
+        }
+    }
+}
---
+++ ФормаСписка/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ФормаСписка
+{
+    internal class UserInfo
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public UserInfo (string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
