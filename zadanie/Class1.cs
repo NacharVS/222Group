69b3674f6dae958bd1e97479482fb0fa27ec23d2
Blur72 <adel.shaihutdinov123@gmail.com>
2024-01-23 12:05:27 +0300

    закончу позже

--- zadanie/Class1.cs
+++ zadanie/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
 
 namespace zadanie
 {
-    internal class Class1
+    internal class logins
     {
+        public logins(string nama, string famile)
+        {
+            this.nama = nama;
+            this.famile = famile;
+        }
+
+        public string nama { get; set; }
+        public string famile { get; set; }
     }
 }
--- zadanie/MainWindow.xaml.cs
+++ zadanie/MainWindow.xaml.cs
@@ -20,9 +20,47 @@ namespace zadanie
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<logins> listlog;
+        List<string> comandos;
+        logins user;
         public MainWindow()
         {
             InitializeComponent();
+            this.listlog = new List<logins>();
+            this.comandos = new List<string>();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void listchistis()
+        {
+            logins228.Items.Clear();
+            foreach (logins umama in listlog)
+            {
+                logins228.Items.Add(umama.nama);
+            }
+        }
+
+        private void reg_Click(object sender, RoutedEventArgs e)
+        {
+            listlog.Add(new logins(namme.Text, surnamme.Text));
+            surnamme.Clear();
+            namme.Clear();
+            listchistis();
+        }
+
+        private void logins228_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            commanda.Items.Add(logins228.SelectedItem);
+            logins228.Items.Remove(logins228.SelectedItem);   
+        }
+
+        private void commanda_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            logins228.Items.Add(commanda.SelectedItem);
+            commanda.Items.Remove(commanda.SelectedItem);
         }
     }
 }
