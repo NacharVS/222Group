994d5cd3dbea23f5e2e6ae35ca9bdf44c7eeb697
KamilIshkeev <148053974+KamilIshkeev@users.noreply.github.com>
2024-01-26 20:50:23 +0300

    Add files via upload

---
+++ Archer.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitsDrafts
+{
+
+    internal class Archer : Unit
+    {
+        private int _speed;
+
+        public Archer() : base("Arch1", 16, 5, 7, 3)
+        {
+        }
+
+        
+
+        public override void BaseInfo()
+        {
+            if (Health < 16)
+            {
+                _speed += 4;
+            }
+            else
+            {
+                _speed = 11;
+            }
+            Console.WriteLine($"Name:{Name} Health: {Health}/{MaxHealth} Speed: {_speed} ");
+        }
+    }
+}
---
+++ Weapon.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitsDrafts
+{
+    internal class Weapon
+    {
+        public Weapon(int minDamage, int maxDamage, int attackSpeed, int accuracy, int durability)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            AttackSpeed = attackSpeed;
+            Accuracy = accuracy;
+            Durability = durability;
+        }
+
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+
+        public int AttackSpeed { get; set; }
+        public int Accuracy { get; set; }
+        public int Durability { get; set; }
+
+        public virtual int Hit()
+        {
+            var x = new Random().Next(1, 101);
+            if (x <= Accuracy)
+            {
+                return new Random().Next(MinDamage, MaxDamage + 1);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
