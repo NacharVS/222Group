7f7624e0cf63c2b16f94dcdc344995e2e19f630d
Данила Сиразаев <danilasirazaev@gmail.com>
2024-01-26 20:37:00 +0300

    46345

---
+++ WpfApp2/HellaPlaya.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace WpfApp2
+//{
+//    internal class HellaPlaya
+//    {
+//        public HellaPlaya (string login, string password)
+//        {
+//            Login = login;
+//            Password = password;
+//        }
+
+//        public string Login { get; set; }
+//        public string Password { get; set; }
+
+//        public static List<MainWindow> MainWindow = new List<MainWindow>();
+//        public void add(TextBox textBox1, TextBox password1) 
+//        {
+//            MainWindow p = new MainWindow
+//            {
+//                Login = textBox1.Text,
+//                Password = password1.Text
+
+//            };
+//            Persons.Add(p);
+//        }
+//    }
+
+
+//    AddList MyList = new AddList();
+//    private void button1_Click(object sender, EventArgs e)
+//    {
+//        MyList.add(login1, password1);
+//    }
+
+
+//}
---
+++ WpfApp2/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    internal class Log
+    {
+
+            public Log(string Login, string Password)
+            {
+                this.Login = Login;
+                this.Password = Password;
+            }
+
+            public string Login { get; set; }
+            public string Password { get; set; }
+
+    }
+}
