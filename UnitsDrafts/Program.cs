154f04c3021dcfe3bc1912aa2b06aeaa734a2ecd
r3dinc <r3dinc@yandex.ru>
2024-02-17 12:55:14 +0300

    equipment

--- UnitsDrafts/Program.cs
+++ UnitsDrafts/Program.cs
@@ -1,18 +1,83 @@
-﻿using TestLibrary;
+﻿using System.Diagnostics;
+using TestLibrary;
+//int j = 0;
+//var tasks = new List<Task>();
+//for (int i = 0; i < 10; i++)
+//{
+//    tasks.Add(new Task(() =>
+//    {
+//        Thread.Sleep(Random.Shared.Next(250, 1500));
+//        Console.WriteLine($"Root of {j} is {Math.Sqrt(j)}");
+//        j++;
+//    }));
+//}
+//foreach (var task in tasks)
+//{
+//    task.Start();
+//}
 
-string[] list = { "Bob", "Sam", "Jeff", "Rob" };
+//Task.WaitAll(tasks.ToArray());
+//Console.WriteLine("Finished");
+//Console.ReadLine();
 
-Pistol pp = new Pistol();
-Rifle rr = new Rifle();
-Machinegun mm = new Machinegun();
+T Find<T>(List<T> l, Predicate<T> pred)
+{
+    for (int i = 0; i < l.Count; i++)
+    {
+        if (pred(l[i]))
+            return l[i];
+    }
+    return default(T);
+}
+
+var x = Enumerable.Range(0, 1000000).Select( i => Random.Shared.Next(0, 1000000));
+var xl = x.ToList();
+Console.WriteLine(default(int));
+Stopwatch sw1 = new Stopwatch();
+Stopwatch sw2 = new Stopwatch();
+Stopwatch sw3 = new Stopwatch();
+Stopwatch sw4 = new Stopwatch();
+
+int iter = 5000;
+for (int i = 0; i < iter; i++)
+{
+    sw1.Start();
+    var y = x.FirstOrDefault(i => i == 950000);
+    sw1.Stop();
+    sw2.Start();
+    var z = x.ToList().Find(i => i == 950000);
+    sw2.Stop();
+    sw3.Start();
+    var w = xl.Find(i => i == 950000);
+    sw3.Stop();
+    sw4.Start();
+    var r = Find(xl, i => i == 950000);
+    sw4.Stop();
+}
+sw1.Stop();
+sw2.Stop();
+Console.WriteLine($"{sw1.Elapsed / (double)iter} {sw2.Elapsed / (double)iter}  {sw3.Elapsed / (double)iter} {sw4.Elapsed / (double)iter}");
 
 
-UseBurstShooting(mm);
 
-//peasant.HealthDecreasedEvent += MethodVar1;
 
-//peasant.HealthIncreasedEvent += MethodVar2;
-//
+
+
+//string[] list = { "Bob", "Sam", "Jeff", "Rob" };
+
+//Pistol pp = new Pistol();
+//Rifle rr = new Rifle();
+//Machinegun mm = new Machinegun();
+
+
+//UseBurstShooting(mm);
+
+////peasant.HealthDecreasedEvent += MethodVar1;
+
+////peasant.HealthIncreasedEvent += MethodVar2;
+////
+///
+
 
 
 
@@ -35,7 +100,7 @@ static (int, string) GetNameAge()
     int a = 10;
     string b = "Dima";
     return (a, b);
-    
+
 }
 
 //Console.WriteLine(a.Item1.GetType().ToString());
